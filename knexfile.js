939b0cb8dde9cfd9aa43b75755a7e048d2cb3676
nick migel <nickmigel2@gmail.com>
2020-04-25 21:01:49 -0400

    changed the users model file to follow the correct path

--- knexfile.js
+++ knexfile.js
@@ -23,10 +23,6 @@ module.exports = {
       user: 'username',
       password: 'password'
     },
-    pool: {
-      min: 2,
-      max: 10
-    },
     migrations: {
       tableName: 'knex_migrations'
     }
