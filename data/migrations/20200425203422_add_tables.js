a18a33e2eb77305d3e0ba0459c31bbc04af4fd59
nick migel <nickmigel2@gmail.com>
2020-04-25 20:56:00 -0400

    added migration and development database

--- data/migrations/20200425203422_add_tables.js
+++ data/migrations/20200425203422_add_tables.js
@@ -1,8 +1,46 @@
 
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+    return knex.schema
+        .createTable('users', tbl => {
+            tbl.increments()
+            tbl.string('username').notNullable().unique()
+            tbl.string('password').notNullable()
+        })
+        .createTable('strains', tbl => {
+            tbl.increments()
+            tbl.string('strain').notNullable().unique()
+            tbl.string('effect').notNullable()
+            tbl.string('medical_effect').notNullable()
+            tbl.string('flavor').notNullable()
+            tbl.string('type').notNullable()
+            tbl.string('thc').notNullable()
+            tbl.string('cbd').notNullable()
+            tbl.string('description').notNullable()
+        })
+        .createTable('saved', tbl => {
+            tbl.increments()
+            tbl.string('strain').notNullable().unique()
+            tbl.string('effect').notNullable()
+            tbl.string('medical_effect').notNullable()
+            tbl.string('flavor').notNullable()
+            tbl.string('type').notNullable()
+            tbl.string('thc').notNullable()
+            tbl.string('cbd').notNullable()
+            tbl.string('description').notNullable()
+            tbl.float('score').notNullable()
+            tbl.integer('reccomendation').notNullable()
+
+            tbl.integer('user_id').notNullable()
+                .references('id')
+                .inTable('users')
+                .onDelete('CASCADE')
+                .onUpdate('CASCADE')
+        })
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+    return knex.schema
+        .dropTableIfExists('saved')
+        .dropTableIfExists('strains')
+        .dropTableIfExists('users')
 };
--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
+      filename: './data/med-cabinet.db3'
     },
     useNullAsDefault: true,
     migrations: {
