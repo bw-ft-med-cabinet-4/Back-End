cbb9c014e338fd9b4f993f667737f601ef293d93
Kevin <knphillips01@gmail.com>
2020-04-26 20:12:50 -0500

    ..

--- api/server.spec.js
+++ api/server.spec.js
@@ -1,9 +1,7 @@
 const request = require('supertest');
 const server = require('./server');
+const db = require("../data/db-Config.js");
 
-it("should set the db env to testing", () => {
-    expect(process.env.DB_ENV).toBe('testing')
-})
 describe('server', () => {
     describe('GET /', () => {
         it('should return status 200 ok', async () => {
@@ -14,13 +12,6 @@ describe('server', () => {
             })
         })
 
-        it('should not return status 500 ok', async () => {
-            return request(server)
-            .get('/')
-            .then(res => {
-                expect(res.status).toBe(200)
-            })
-        })
-
+ 
     })
 })
